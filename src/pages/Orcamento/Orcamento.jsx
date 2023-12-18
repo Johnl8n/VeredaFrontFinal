56c45b1f63b68d1160771b693c0ed83746f96d66
Sávio Rodrigues <capar.2022113tads0049@aluno.ifpi.edu.br>
2023-12-18 15:05:35 -0300

    feat: add pdf

--- src/pages/Orcamento/Orcamento.jsx
+++ src/pages/Orcamento/Orcamento.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-
-import NavBar from "../../components/Elements/NavBar";
-import style from "./Orcamento.module.css";
-
 import { BiShow } from "react-icons/bi";
 import { MdOutlineEdit } from "react-icons/md";
 import { FaRegTrashAlt } from "react-icons/fa";
-
-import api from "../../utils/api";
 import { useNavigate } from "react-router-dom";
+import NavBar from "../../components/Elements/NavBar";
+import style from "./Orcamento.module.css";
+import api from "../../utils/api";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const Orcamento = () => {
-  const navegate = useNavigate();
+  const navigate = useNavigate();
   const [idsProdutos, setIdsProdutos] = useState([]);
   const [produtosOrcados, setProdutosOrcados] = useState([]);
   const [valorTotal, setValorTotal] = useState(0);
@@ -64,21 +65,18 @@ const Orcamento = () => {
   }, [idsProdutos]);
 
   const verProduto = (id) => {
-    navegate(`/produto/${id}`);
+    navigate(`/produto/${id}`);
   };
 
   const removeProduto = (id) => {
     try {
-      const produtosFiltrados = produtosOrcados.filter((produto) => {
-        return produto.id != id;
-      });
+      const produtosFiltrados = produtosOrcados.filter((produto) => produto.id != id);
 
       setProdutosOrcados(produtosFiltrados);
 
       toast.success("Produto removido ");
 
-      const local_id =
-        JSON.parse(localStorage.getItem("ProdutosOrcados")) || [];
+      const local_id = JSON.parse(localStorage.getItem("ProdutosOrcados")) || [];
       const local_filtrado = local_id.filter((element) => element.id != id);
       localStorage.setItem("ProdutosOrcados", JSON.stringify(local_filtrado));
       setIdsProdutos(local_filtrado);
@@ -87,12 +85,69 @@ const Orcamento = () => {
     }
   };
 
+  const gerarPDF = () => {
+    // const logo = "logonew.png";
+    
+    const docDefinition = {
+      // background: [
+      //   {
+      //     image: logo,
+      //     width: 300,
+      //   }
+      // ],
+      content: [
+        { text: "Meu orçamento", style: "header" },
+        {
+          table: {
+            headerRows: 1,
+            widths: ['*', 'auto', 'auto', 'auto', 'auto', 'auto'],
+            body: [
+              [{ text: 'Fornecedor', style: 'tableHeader' },
+               { text: 'Situação', style: 'tableHeader' },
+               { text: 'Quantidade', style: 'tableHeader' },
+               { text: 'Valor', style: 'tableHeader' },
+               { text: 'Data', style: 'tableHeader' },
+               { text: 'Material', style: 'tableHeader' }],
+              ...produtosOrcados.map((produto, index) => [
+                produto.empresa.nome,
+                "Em Estoque",
+                idsProdutos[index].quantidade,
+                { text: `R$ ${produto.preco}`, alignment: 'right' },
+                new Date().toLocaleDateString(),
+                produto.produto.nome,
+              ]),
+            ],
+          },
+        },
+        { text: `\nTotal: R$ ${valorTotal.toFixed(2)}`, style: "total" },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 13,
+          color: "black",
+        },
+        total: {
+          fontSize: 14,
+          bold: true,
+          margin: [0, 10, 0, 0],
+        },
+      },
+    };
+
+    pdfMake.createPdf(docDefinition).open();
+  };
+
   return (
     <div className={style.body}>
       <NavBar />
-
       <div className={style.container}>
-        <h2>Meus Orcamentos</h2>
+        <h2>Meus Orçamentos</h2>
         <div className={style.div_table}>
           <table className={style.table}>
             <thead>
@@ -107,33 +162,30 @@ const Orcamento = () => {
               </tr>
             </thead>
             <tbody>
-              {console.log(produtosOrcados)}
               {produtosOrcados.length > 0 &&
-                produtosOrcados.map((produto, index) => {
-                  return (
-                    <tr key={produto.id}>
-                      <td>{produto.empresa.nome}</td>
-                      <td>Em Estoque</td>
-                      <td>{idsProdutos[index].quantidade}</td>
-                      <td>{produto.preco}</td>
-                      <td>{new Date().toLocaleDateString()}</td>
-                      <td>{produto.produto.nome}</td>
-                      <td className={style.actions}>
-                        <BiShow onClick={() => verProduto(produto.id)} />
-                        <MdOutlineEdit values={produto.id} />
-                        <FaRegTrashAlt
-                          onClick={() => removeProduto(produto.id)}
-                        />
-                      </td>
-                    </tr>
-                  );
-                })}
+                produtosOrcados.map((produto, index) => (
+                  <tr key={produto.id}>
+                    <td>{produto.empresa.nome}</td>
+                    <td>Em Estoque</td>
+                    <td>{idsProdutos[index].quantidade}</td>
+                    <td>{produto.preco}</td>
+                    <td>{new Date().toLocaleDateString()}</td>
+                    <td>{produto.produto.nome}</td>
+                    <td className={style.actions}>
+                      <BiShow onClick={() => verProduto(produto.id)} />
+                      <MdOutlineEdit values={produto.id} />
+                      <FaRegTrashAlt onClick={() => removeProduto(produto.id)} />
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
         <div className={style.div_infos}>
-          <p>R$ {valorTotal}</p>
-          <button className={style.button_save}>Salvar</button>
+          <p>R$ {valorTotal.toFixed(2)}</p>
+          <button onClick={gerarPDF} className={style.button_save} type="button">
+            Gerar PDF <img src="public/file-pdf-solid.svg" alt="" />
+          </button>
         </div>
       </div>
     </div>
